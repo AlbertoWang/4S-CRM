1f760f0817b3eb9a584a7a8657a6d60a9731c279
AlbertoWang <albertowang@foxmail.com>
2020-04-22 13:42:20 +0800

    Init push

---
+++ CRM/src/main/java/cn/edu/cqu/CRM/Service/ServiceImpl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.edu.cqu.CRM.Service.ServiceImpl;
+
+import org.springframework.stereotype.Component;
+
+import cn.edu.cqu.CRM.Service.UserService;
+
+@Component
+public class UserServiceImpl implements UserService {
+
+}
